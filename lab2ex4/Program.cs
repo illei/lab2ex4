9669eefdf29d15c85496d3ad73b5da17e177e195
Illei Adrian <illei.adrian@yahoo.com>
2023-09-06 16:44:30 +0200

    Add project files.

---
+++ lab2ex4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace lab2ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*: Scrieti un program care va determina daca un numar este sau nu
+        palindrom.*/
+            string inversul="";
+            int conversie;
+
+            int x = int.Parse(Console.ReadLine());
+            int y = x;/*palceholder*/
+            while (x != 0)
+            {
+                 inversul = inversul + x % 10;
+
+                x = x / 10;
+
+
+            }
+
+            
+            Console.WriteLine("inversul este : " + inversul);
+            conversie = Int16.Parse(inversul);
+            if (y == conversie)
+            {
+                Console.WriteLine("este plaindrom");
+            }
+
+        }
+    }
+}
